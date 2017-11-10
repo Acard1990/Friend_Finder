041b899180af548d24c59fcd7784b6fa836deaa5
Scott Cardinali <ScottCardinali@scotts-mbp.attlocal.net>
2017-11-10 11:06:09 -0500

    debugging

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,12 @@
-const friends = require('../data/friends');
-//const NewFriend = require('../data/newFriend');
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-  app.get('/api/friends', function(req, res){
-    res.json(friends);
-  });
-  app.post('/api/friends', function(req, res){
-    var newFriend = new NewFriend(req.body);
-    friends.push(req.body);
-    console.log(newFriend.friend);
-    res.json(newFriend.friend);
-  });
+    app.post("/api/friends", function (req, res) {
+        var newfriend = req.body;
+        friends.push(newfriend);
+        res.json(newfriend)
+    });
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
 };
