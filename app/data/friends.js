60d1c4ea24e01c9a7b62c4ab20421407bbe11481
Scott Cardinali <ScottCardinali@scotts-mbp.attlocal.net>
2017-11-10 10:33:04 -0500

    built htmls and css logic seems to work

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+const friends = [
+  {
+    "name": "Jen",
+    "photo": "images/jen.jpg",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    "name": "Tim",
+    "photo": "images/tim.jpg",
+    "scores": [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  },
+  {
+    "name": "Wyatt",
+    "photo": "images/wyatt.jpg",
+    "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    "name": "Elon",
+    "photo": "images/elon.jpg",
+    "scores": [1, 2, 3, 4, 5, 4, 3, 2, 1, 1]
+  }
+];
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
